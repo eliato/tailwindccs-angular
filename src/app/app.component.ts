2c6013c94ba39ee645700fa0ba34d97a69645671
Elias Torres <soyeliastorres@gmail.com>
2022-05-22 16:43:00 -0600

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tail-ag';
+
+
+  ngOnInit(): void {
+   const menuButton = document.querySelector('#mobile-menu')
+  menuButton?.addEventListener('click', e => {
+    const menu = document.querySelector('.mobile-links')
+    menu?.classList.toggle('hidden')
+  })
+  }
+  
+
+   
+
+
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,20 @@ module.exports = {
   },
   darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: (theme) => ({
+        "wave-pattern": "url(src/assets/img/wave-white.png)",
+        "wave-pattern-dots": "url(src/assets/img/pattern-white-dots.png)",
+        "pattern-characters-red":
+          "url(src/assets/img/pattern-characters-red.png)",
+        "character-mario": "url(src/assets/img/character-mario.png)",
+        "wave-pink": "url(src/assets/img/wave-pink.png)",
+        "yellow-dots": "url(src/assets/img/yellow-dots.png)",
+      }),
+      screens: {
+        xsm: "320px",
+      },
+    },
   },
   variants: {
     extend: {},
